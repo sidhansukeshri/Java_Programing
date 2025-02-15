c26167b91c13ed23febd979c03d5e814c027344c
Sidhansu Keshri <112798263+sidhansukeshri@users.noreply.github.com>
2025-02-15 23:24:25 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,13 @@
+public class demo1 {
+    static int demo(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        int c = demo(a,b);
+        System.out.println("Sum of a and b is: "+c);
+    }
+
+    
+}
