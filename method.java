acefc08cdc8cf2d91193730d6458aee283354686
Sidhansu Keshri <112798263+sidhansukeshri@users.noreply.github.com>
2025-02-15 23:25:37 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,11 @@
+class demo{
+    public void welcome(){
+        System.out.println("Welcome To Java.");
+    }
+}
+class example{
+    public static void main(String[] args) {
+        demo obj = new demo();
+        obj .welcome();
+    }
+}
