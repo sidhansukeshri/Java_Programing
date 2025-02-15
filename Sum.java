15b1137f6c397c10e0eb102b7a5bb249a65fd024
Sidhansu Keshri <112798263+sidhansukeshri@users.noreply.github.com>
2025-02-15 23:27:15 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+class sum{
+    public void add(){
+        int a=10,b=5;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
+class example{
+    public static void main(String[] args) {
+        sum obj = new sum();
+        obj.add();
+    }
+}
