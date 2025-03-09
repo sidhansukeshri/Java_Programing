8d60efcc1cafb3bfbe0a198085ea8244d41d6a33
Sidhansu Keshri <112798263+sidhansukeshri@users.noreply.github.com>
2025-03-09 19:45:53 +0530

    Create 1.java

---
+++ NPTEL/Week1/1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class W01_P1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+// Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+        
+
+in.close();
+    }
+}
